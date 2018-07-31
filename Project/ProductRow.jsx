55217b09ba4e051ac38c1d8994c74e052199030c
Michael Allen <mike@mistColoured.com>
2018-07-31 10:28:44 +0100

    Initial commit. Refactored from original source.

---
+++ Project/ProductRow.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable */
+
+import React, { Component } from 'react';
+
+class ProductRow extends React.Component {
+  render() {
+    const product = this.props.product;
+    const name = product.stocked
+      ? product.name
+      : (
+<span style={{ color: 'red' }}>
+        {product.name}
+      </span>
+);
+
+    return (
+      <tr>
+        <td>{name}</td>
+        <td>{product.price}</td>
+      </tr>
+    );
+  }
+}
+
+export default ProductRow
+;
